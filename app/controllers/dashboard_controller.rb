627d010e7ab06a83a9ff6dfe39acf711d425ebf4
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2015-06-20 00:15:54 +0530

    Initial comit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  
+  def home
+    render :text => "Hello World!"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IWasHere::Application.config.secret_token = 'd19db87efc4610959472aae6a91d2b89d4a63a38945e3e643e5dbf38a9be7f13e98c36dab75a16ff6626126f250361a2b109e59de066ddea996256b4ad3046ac'
